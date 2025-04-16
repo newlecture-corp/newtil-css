e89ffabd061be7cd34fd96813dc965904fb4db16
newlec <newlec@newlecture.com>
2025-04-16 14:01:01 +0900

    [chore] border 공통분모 수정

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // CSS 파일을 가져옴
-import "./dist/utils.css";
-import "./dist/components.css";
+// import "./dist/utils.css";
+// import "./dist/components.css";
+import "./dist/style.css";
 
 // JavaScript 모듈을 가져옴
 export { default as initLayout } from "./dist/js/n-layout";
