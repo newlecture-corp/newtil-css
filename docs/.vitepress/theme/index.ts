c929da10f4f5b0b722d32cfdb69ea7eaf65f51d5
newlec <newlec@namoolab.com>
2025-11-07 18:16:37 +0900

    fix(docs): point VitePress theme to css/style.css

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import '../../../dist/style.css';
+import '../../../css/style.css';
 import './style.css';
 
 export default {
