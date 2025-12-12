53e679c67c9fc997680b4ff650d1381cf870c737
황서범 <wlgns1251@naver.com>
2025-12-12 12:10:35 +0900

    드래그 수정

--- css/component/m3/m3-card.js
+++ css/component/m3/m3-card.js
@@ -1,21 +1,13 @@
 function onEdit() {
-    alert("편집 버튼이 클릭되었습니다!");
+  alert("편집 버튼이 클릭되었습니다!");
 }
 
-const card = document.querySelector('.m3-card');
-card.addEventListener('dragstart', () => {
-  card.classList.add('drag');
+const cards = document.querySelectorAll('.m3-card, .m3-card-horizontal');
+cards.forEach(function (card) {
+  card.addEventListener('dragstart', function () {
+    card.classList.add('drag');
+  });
+  card.addEventListener('dragend', function () {
+    card.classList.remove('drag');
+  });
 });
-card.addEventListener('dragend', () => {
-  card.classList.remove('drag');
-});
-
-
-const card2 = document.querySelector('.m3-card-horizontal');
-card2.addEventListener('dragstart', () => {
-  card2.classList.add('drag');
-});
-card2.addEventListener('dragend', () => {
-  card2.classList.remove('drag');
-});
-
