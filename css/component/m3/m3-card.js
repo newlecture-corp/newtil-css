17c90f73b9dd823ef72570aee0a91080fcdd98dd
황서범 <wlgns1251@naver.com>
2025-12-17 20:04:30 +0900

    카드.js 수정

--- css/component/m3/m3-card.js
+++ css/component/m3/m3-card.js
@@ -1,14 +1,3 @@
 function onEdit() {
     alert("편집 버튼이 클릭되었습니다!");
 }
-
-const cards = document.querySelectorAll(".m3-card, .horizontal");
-cards.forEach(function (card) {
-    card.setAttribute("draggable", "true");
-    card.addEventListener("dragstart", function () {
-        card.classList.add("drag");
-    });
-    card.addEventListener("dragend", function () {
-        card.classList.remove("drag");
-    });
-});
